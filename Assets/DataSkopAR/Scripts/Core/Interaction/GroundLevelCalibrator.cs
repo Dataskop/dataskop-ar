428fcdffd16dd0b3d6fd719c45ac29dd0d8e8333
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-23 10:02:00 +0100

    implement calibration interface #1

--- Assets/DataSkopAR/Scripts/Core/Interaction/GroundLevelCalibrator.cs
+++ Assets/DataSkopAR/Scripts/Core/Interaction/GroundLevelCalibrator.cs
@@ -8,7 +8,14 @@ namespace DataskopAR.Interaction {
 	/// <summary>
 	///     Responsible for aligning the AR Worlds y-Axis to the real worlds ground level.
 	/// </summary>
-	public class GroundLevelCalibrator : MonoBehaviour {
+	public class GroundLevelCalibrator : MonoBehaviour, ICalibration {
+
+#region Events
+
+		[Header("Events")]
+		[SerializeField] private UnityEvent groundDetected;
+
+#endregion
 
 #region Fields
 
@@ -16,29 +23,26 @@ namespace DataskopAR.Interaction {
 		[SerializeField] private ARPlaneManager arPlaneManager;
 		[SerializeField] private AbstractMap map;
 
-		[Header("Events")]
-		[SerializeField] private UnityEvent groundDetected;
-
 #endregion
 
 #region Properties
 
 		private float GroundLevelYPosition { get; set; }
-		private bool IsCalibrating { get; set; }
+		public bool IsEnabled { get; set; }
 		private bool HasCalibrated { get; set; }
 
 #endregion
 
 #region Methods
 
-		private void Start() {
+		private void OnEnable() {
 			arPlaneManager.planesChanged += GetLowestPlane;
 			map.OnUpdated += OnMapUpdated;
 		}
 
 		private void GetLowestPlane(ARPlanesChangedEventArgs e) {
 
-			if (!IsCalibrating)
+			if (!IsEnabled)
 				return;
 
 			foreach (ARPlane plane in e.added) {
@@ -64,34 +68,24 @@ namespace DataskopAR.Interaction {
 		}
 
 		private void SetRootGroundLevel(float newGroundLevel) {
+
 			Vector3 mapPosition = map.Root.position;
 			mapPosition = new Vector3(mapPosition.x, newGroundLevel, mapPosition.z);
 			map.Root.position = mapPosition;
+
 		}
 
 		private void ResetRootGroundLevel() {
 
 			GroundLevelYPosition = 0;
-
 			Vector3 mapPosition = map.Root.position;
 			mapPosition = new Vector3(mapPosition.x, 0, mapPosition.z);
 			map.Root.position = mapPosition;
+
 		}
 
 		private void OnMapUpdated() {
 			SetRootGroundLevel(GroundLevelYPosition);
-		}
-
-		public void ToggleGroundLevelCalibration() {
-			IsCalibrating = !IsCalibrating;
-			TogglePlanes(IsCalibrating);
-			arPlaneManager.enabled = IsCalibrating;
-		}
-
-		public void SetGroundLevelCalibrationState(bool newState) {
-			IsCalibrating = newState;
-			TogglePlanes(IsCalibrating);
-			arPlaneManager.enabled = IsCalibrating;
 		}
 
 		public void ResetCalibratedStatus() {
@@ -104,6 +98,23 @@ namespace DataskopAR.Interaction {
 			foreach (ARPlane plane in arPlaneManager.trackables) {
 				plane.gameObject.SetActive(status);
 			}
+
+		}
+
+		public ICalibration Enable() {
+
+			IsEnabled = true;
+			TogglePlanes(IsEnabled);
+			arPlaneManager.enabled = IsEnabled;
+			return this;
+
+		}
+
+		public void Disable() {
+
+			IsEnabled = false;
+			TogglePlanes(IsEnabled);
+			arPlaneManager.enabled = IsEnabled;
 
 		}
 
