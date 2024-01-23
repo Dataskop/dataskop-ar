90fcba182307d7d6df0dcd0b5ba41bad1f706e61
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2024-01-23 16:16:01 +0100

    add calibration phase texts

--- Assets/DataSkopAR/Scripts/Core/UI/Calibrator/CalibratorTextRepository.cs
+++ Assets/DataSkopAR/Scripts/Core/UI/Calibrator/CalibratorTextRepository.cs
@@ -5,14 +5,18 @@ namespace DataskopAR.UI {
 
 	public static class CalibratorTextRepository {
 
+#region Constants
+		
+		// @formatter:off
+
 		public static readonly Dictionary<CalibratorPhase, string> CalibratorGuideDict = new() {
-			{ CalibratorPhase.Initial, "To guarantee the best experience, please calibrate the application first!" },
-			{ CalibratorPhase.NorthAlignStart, "" },
-			{ CalibratorPhase.NorthAlignFinish, "" },
-			{ CalibratorPhase.GroundStart, "Please try to align the GREEN rectangle on the lowest point (the floor normally) where you are standing." },
-			{ CalibratorPhase.GroundFinish, "Ground Calibrated!" },
-			{ CalibratorPhase.RoomStart, ""},
-			{ CalibratorPhase.RoomFinish, ""},
+			{ CalibratorPhase.Initial, "Welcome! Let's fine-tune your experience. Start  the calibration now for optimal performance!" },
+			{ CalibratorPhase.NorthAlignStart, "Ready to align? Turn with your device until the compass lines up with North." },
+			{ CalibratorPhase.NorthAlignFinish, "North alignment complete! Ready for the next step." },
+			{ CalibratorPhase.GroundStart, "Grounding your view. Align the GREEN rectangle with the floor at your feet to continue." },
+			{ CalibratorPhase.GroundFinish, "Ground calibration successful! You're all set for the next stage." },
+			{ CalibratorPhase.RoomStart, "Time to capture your surroundings! Slowly scan the room with your device. A progress bar will fill up as the scan improves - the fuller, the better." },
+			{ CalibratorPhase.RoomFinish, "Room mapping is complete! Just one more step to go." },
 			{ CalibratorPhase.End, "Done! Click on the 'Finish' button to continue to the main application." }
 		};
 
@@ -22,10 +26,14 @@ namespace DataskopAR.UI {
 			{ CalibratorPhase.NorthAlignFinish, "Continue" },
 			{ CalibratorPhase.GroundStart, "Continue" },
 			{ CalibratorPhase.GroundFinish, "Continue" },
-			{ CalibratorPhase.RoomStart, "Continue"},
-			{ CalibratorPhase.RoomFinish, "Continue"},
+			{ CalibratorPhase.RoomStart, "Continue" },
+			{ CalibratorPhase.RoomFinish, "Continue" },
 			{ CalibratorPhase.End, "Finish" }
 		};
+		
+		// @formatter:on
+
+#endregion
 
 	}
 
