6a58b8469016ebdc60d6588a13c487561eea4304
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2024-01-23 16:17:35 +0100

    fix wrong calibrator text

--- Assets/DataSkopAR/Scripts/Core/UI/Calibrator/CalibratorTextRepository.cs
+++ Assets/DataSkopAR/Scripts/Core/UI/Calibrator/CalibratorTextRepository.cs
@@ -10,14 +10,14 @@ namespace DataskopAR.UI {
 		// @formatter:off
 
 		public static readonly Dictionary<CalibratorPhase, string> CalibratorGuideDict = new() {
-			{ CalibratorPhase.Initial, "Welcome! Let's fine-tune your experience. Start  the calibration now for optimal performance!" },
+			{ CalibratorPhase.Initial, "Welcome! Let's fine-tune your experience. Start the calibration now for optimal performance!" },
 			{ CalibratorPhase.NorthAlignStart, "Ready to align? Turn with your device until the compass lines up with North." },
 			{ CalibratorPhase.NorthAlignFinish, "North alignment complete! Ready for the next step." },
 			{ CalibratorPhase.GroundStart, "Grounding your view. Align the GREEN rectangle with the floor at your feet to continue." },
 			{ CalibratorPhase.GroundFinish, "Ground calibration successful! You're all set for the next stage." },
 			{ CalibratorPhase.RoomStart, "Time to capture your surroundings! Slowly scan the room with your device. A progress bar will fill up as the scan improves - the fuller, the better." },
 			{ CalibratorPhase.RoomFinish, "Room mapping is complete! Just one more step to go." },
-			{ CalibratorPhase.End, "Done! Click on the 'Finish' button to continue to the main application." }
+			{ CalibratorPhase.End, "All set! Hit 'Finish' to dive into the full application experience." }
 		};
 
 		public static readonly Dictionary<CalibratorPhase, string> CalibratorButtonTextDict = new() {
