85b7cf993cc02e218f79bfa67c0754b12a0c6582
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-01-25 14:50:46 +0100

    add ground detection with user input

--- Assets/DataSkopAR/Scripts/Core/Interaction/GroundLevelCalibrator.cs
+++ Assets/DataSkopAR/Scripts/Core/Interaction/GroundLevelCalibrator.cs
@@ -1,7 +1,7 @@
 using System;
 using Mapbox.Unity.Map;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.ARFoundation;
 
 namespace DataskopAR.Interaction {
@@ -10,6 +10,12 @@ namespace DataskopAR.Interaction {
 	///     Responsible for aligning the AR Worlds y-Axis to the real worlds ground level.
 	/// </summary>
 	public class GroundLevelCalibrator : MonoBehaviour, ICalibration {
+
+#region Constants
+
+		private const int TargetLayerMask = 1 << 10;
+
+#endregion
 
 #region Events
 
@@ -22,14 +28,17 @@ namespace DataskopAR.Interaction {
 		[Header("References")]
 		[SerializeField] private ARPlaneManager arPlaneManager;
 		[SerializeField] private AbstractMap map;
+		[SerializeField] private Camera camera;
 
 #endregion
 
 #region Properties
 
 		public float GroundLevelYPosition { get; set; }
+		public ARPlane LowestPlane { get; private set; }
 		public bool IsEnabled { get; set; }
-		private bool HasCalibrated { get; set; }
+		private Ray TapScreenToWorldRay { get; set; }
+		private Vector2 TapPosition { get; set; }
 
 #endregion
 
@@ -43,6 +52,7 @@ namespace DataskopAR.Interaction {
 		public ICalibration Enable() {
 
 			IsEnabled = true;
+			camera = Camera.main;
 			arPlaneManager.enabled = IsEnabled;
 			TogglePlanes(IsEnabled);
 			return this;
@@ -72,6 +82,7 @@ namespace DataskopAR.Interaction {
 
 			if (GroundLevelYPosition > yPos) {
 				GroundLevelYPosition = yPos;
+				LowestPlane = foundPlane;
 			}
 
 		}
@@ -101,6 +112,39 @@ namespace DataskopAR.Interaction {
 
 			foreach (ARPlane plane in arPlaneManager.trackables) {
 				plane.gameObject.SetActive(status);
+			}
+
+		}
+
+		public void TapPositionInput(InputAction.CallbackContext ctx) {
+
+			if (IsEnabled) {
+				TapPosition = ctx.ReadValue<Vector2>();
+			}
+
+		}
+
+		public void TapInput(InputAction.CallbackContext ctx) {
+
+			if (ctx.canceled) {
+
+				if (!IsEnabled) {
+					return;
+				}
+
+				TapScreenToWorldRay = camera.ScreenPointToRay(new Vector3(TapPosition.x, TapPosition.y, -5));
+
+				if (Physics.Raycast(TapScreenToWorldRay, out RaycastHit hit, Mathf.Infinity, TargetLayerMask)) {
+
+					if (hit.collider.gameObject.CompareTag("ARPlane")) {
+
+						SetMapRootGroundLevel(LowestPlane.transform.position.y);
+						CalibrationCompleted?.Invoke();
+
+					}
+
+				}
+
 			}
 
 		}
