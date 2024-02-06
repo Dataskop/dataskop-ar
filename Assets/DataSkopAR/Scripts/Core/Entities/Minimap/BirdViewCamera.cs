3bee401deb87b1465639f89871152a9ea2f1aa1a
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2024-02-06 11:12:26 +0100

    implement bird view camera methods

--- Assets/DataSkopAR/Scripts/Core/Entities/Minimap/BirdViewCamera.cs
+++ Assets/DataSkopAR/Scripts/Core/Entities/Minimap/BirdViewCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using Mapbox.Unity.Map;
 using UnityEngine;
 
@@ -7,21 +8,43 @@ namespace DataskopAR.Entities {
 
 #region Fields
 
+		[Header("References")]
 		[SerializeField] private AbstractMap map;
 		[SerializeField] private Camera birdViewCamera;
 		[SerializeField] private Transform userCameraTransform;
+
+		[Header("Values")]
+		[SerializeField] private float defaultCameraSize = 20;
 
 #endregion
 
 #region Methods
 
+		private void Awake() {
+			SetCameraSize(defaultCameraSize);
+		}
+
 		private void FixedUpdate() {
+			birdViewCamera.transform.SetPositionAndRotation(
+				GetTrackedPosition(userCameraTransform.position),
+				GetAlignedRotation(map.transform.rotation)
+			);
+		}
 
-			Vector3 nextPos = new(userCameraTransform.position.x, 20, userCameraTransform.position.z);
-			Quaternion nextRotation = Quaternion.Euler(new Vector3(90, map.transform.rotation.eulerAngles.y, 0));
+		private Vector3 GetTrackedPosition(Vector3 trackingPosition) {
+			return new Vector3(trackingPosition.x, 20, trackingPosition.z);
+		}
 
-			birdViewCamera.transform.SetPositionAndRotation(nextPos, nextRotation);
+		private Quaternion GetAlignedRotation(Quaternion aligningRotation) {
+			return Quaternion.Euler(new Vector3(90, aligningRotation.eulerAngles.y, 0));
+		}
 
+		public void SetCameraSize(float newSize) {
+			birdViewCamera.orthographicSize = newSize;
+		}
+
+		public void ResetCameraSize() {
+			birdViewCamera.orthographicSize = defaultCameraSize;
 		}
 
 #endregion
