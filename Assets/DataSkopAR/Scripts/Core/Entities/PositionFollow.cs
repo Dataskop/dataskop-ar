c0dbd4f19a2729bb0f088d7a9e4add95c1e7b781
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2024-02-06 10:39:56 +0100

    refactor entity scripts

--- Assets/DataSkopAR/Scripts/Core/Entities/PositionFollow.cs
+++ Assets/DataSkopAR/Scripts/Core/Entities/PositionFollow.cs
@@ -4,22 +4,30 @@ namespace DataskopAR.Entities {
 
 	public class PositionFollow : MonoBehaviour {
 
+#region Fields
+
 		[SerializeField] private Transform targetTransform;
 
 		[SerializeField] private bool freezeX;
 		[SerializeField] private bool freezeY;
 		[SerializeField] private bool freezeZ;
 
+#endregion
+
+#region Methods
+
 		private void FixedUpdate() {
 
 			Vector3 targetPos = targetTransform.position;
-			var xPos = freezeX ? transform.position.x : targetPos.x;
-			var yPos = freezeY ? transform.position.y : targetPos.y;
-			var zPos = freezeZ ? transform.position.z : targetPos.z;
+			float xPos = freezeX ? transform.position.x : targetPos.x;
+			float yPos = freezeY ? transform.position.y : targetPos.y;
+			float zPos = freezeZ ? transform.position.z : targetPos.z;
 
 			transform.position = new Vector3(xPos, yPos, zPos);
 
 		}
+
+#endregion
 
 	}
 
--- Assets/DataSkopAR/Scripts/Core/Entities/RotationLock.cs
+++ Assets/DataSkopAR/Scripts/Core/Entities/RotationLock.cs
@@ -4,21 +4,29 @@ namespace DataskopAR.Entities {
 
 	public class RotationLock : MonoBehaviour {
 
+#region Fields
+
 		[SerializeField] private Transform targetTransform;
 
 		[SerializeField] private bool freezeX;
 		[SerializeField] private bool freezeY;
 		[SerializeField] private bool freezeZ;
 
+#endregion
+
+#region Methods
+
 		private void FixedUpdate() {
 
 			var xEuler = freezeX ? transform.rotation.eulerAngles.x : targetTransform.rotation.eulerAngles.x;
 			var yEuler = freezeY ? transform.rotation.eulerAngles.y : targetTransform.rotation.eulerAngles.y;
 			var zEuler = freezeZ ? transform.rotation.eulerAngles.z : targetTransform.rotation.eulerAngles.z;
-			
+
 			transform.rotation = Quaternion.Euler(xEuler, yEuler, zEuler);
 
 		}
+
+#endregion
 
 	}
 
