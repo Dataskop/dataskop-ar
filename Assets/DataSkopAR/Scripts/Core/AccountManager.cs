3f2cc66b567d97d711ba290b854f93796ef1e0a9
Ramon Brullo <kingvanti650@gmail.com>
2024-02-28 11:56:55 +0100

    refactor to extract method

--- Assets/DataSkopAR/Scripts/Core/AccountManager.cs
+++ Assets/DataSkopAR/Scripts/Core/AccountManager.cs
@@ -5,18 +5,22 @@ namespace DataskopAR {
 	public static class AccountManager {
 
 #region Constants
-		
-			private const string APITokenKey = "API_TOKEN";
+
+		private const string APITokenKey = "API_TOKEN";
 
 #endregion
-		
+
 #region Properties
 
-		public static bool IsLoggedIn => PlayerPrefs.HasKey(APITokenKey) && !string.IsNullOrEmpty(PlayerPrefs.GetString(APITokenKey));
+		public static bool IsLoggedIn => HasToken() && !string.IsNullOrEmpty(PlayerPrefs.GetString(APITokenKey));
 
 #endregion
 
 #region Methods
+
+		private static bool HasToken() {
+			return PlayerPrefs.HasKey(APITokenKey);
+		}
 
 		public static void Login(string loginToken) {
 			PlayerPrefs.SetString(APITokenKey, loginToken);
@@ -24,7 +28,7 @@ namespace DataskopAR {
 
 		public static void Logout() {
 
-			if (PlayerPrefs.HasKey(APITokenKey)) {
+			if (HasToken()) {
 				PlayerPrefs.DeleteKey(APITokenKey);
 			}
 
