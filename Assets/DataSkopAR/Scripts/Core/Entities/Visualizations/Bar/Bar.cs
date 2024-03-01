50d09c09c43de87be183d57d729ca2f50077669f
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2024-03-01 13:46:29 +0100

    fix bar time elements

--- Assets/DataSkopAR/Scripts/Core/Entities/Visualizations/Bar/Bar.cs
+++ Assets/DataSkopAR/Scripts/Core/Entities/Visualizations/Bar/Bar.cs
@@ -29,6 +29,10 @@ namespace DataskopAR.Entities.Visualizations {
 		[SerializeField] private TextMeshProUGUI minValueTextMesh;
 		[SerializeField] private TextMeshProUGUI maxValueTextMesh;
 
+		[Header("Values")]
+		[SerializeField] private float minBarFillHeight;
+		[SerializeField] private float maxBarFillHeight;
+
 		private Vector3 origin;
 
 #endregion
@@ -72,9 +76,9 @@ namespace DataskopAR.Entities.Visualizations {
 			barCollider.enabled = true;
 		}
 
-		private void SetPillarHeight(float heightValue, float minValue, float maxValue) {
+		private void SetPillarHeight(float heightValue, float minValue, float maxValue, float minBarHeight, float maxBarHeight) {
 			heightValue = Mathf.Clamp(heightValue, minValue, maxValue);
-			BarHeight = MathExtensions.Map01(heightValue, minValue, maxValue);
+			BarHeight = MathExtensions.Map(heightValue, minValue, maxValue, minBarHeight, maxBarHeight);
 			Vector3 localScale = barFill.localScale;
 			BarFillScale = new Vector3(localScale.x, BarHeight, localScale.z);
 			localScale = BarFillScale;
@@ -95,11 +99,11 @@ namespace DataskopAR.Entities.Visualizations {
 		}
 
 		private void RotateVisualization() {
-			transform.SetPositionAndRotation(transform.position + new Vector3(0, 0.25f, 0), Quaternion.Euler(new Vector3(0, 0, 90)));
+			// transform.SetPositionAndRotation(transform.position + new Vector3(0, 0.25f, 0), Quaternion.Euler(new Vector3(0, 0, 90)));
 		}
 
 		private void ResetRotation() {
-			transform.SetPositionAndRotation(transform.position - new Vector3(0, 0.25f, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+			// transform.SetPositionAndRotation(transform.position - new Vector3(0, 0.25f, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
 		}
 
 		public override void ApplyStyle(VisualizationStyle style) {
@@ -149,13 +153,15 @@ namespace DataskopAR.Entities.Visualizations {
 			switch (DataPoint.MeasurementDefinition.MeasurementType) {
 				case MeasurementType.Bool: {
 					bool receivedValue = mr.ReadAsBool();
-					SetPillarHeight(receivedValue ? 1f : 0f, DataPoint.Attribute.Minimum, DataPoint.Attribute.Maximum);
+					SetPillarHeight(receivedValue ? 1f : 0f, DataPoint.Attribute.Minimum, DataPoint.Attribute.Maximum, minBarFillHeight,
+						maxBarFillHeight);
 					SetDisplayValue(receivedValue);
 					break;
 				}
 				case MeasurementType.Float: {
 					float receivedValue = mr.ReadAsFloat();
-					SetPillarHeight(receivedValue, DataPoint.Attribute.Minimum, DataPoint.Attribute.Maximum);
+					SetPillarHeight(receivedValue, DataPoint.Attribute.Minimum, DataPoint.Attribute.Maximum, minBarFillHeight,
+						maxBarFillHeight);
 					SetDisplayValue(receivedValue);
 					SetMinMaxDisplayValues(DataPoint.Attribute.Minimum, DataPoint.Attribute.Maximum);
 					break;
