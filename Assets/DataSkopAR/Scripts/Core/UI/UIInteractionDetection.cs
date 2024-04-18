3ab690842883d2cceb35c50e1c3bf4048cf8d0be
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2024-04-18 14:32:27 +0200

    fix ui interaction

--- Assets/DataSkopAR/Scripts/Core/UI/UIInteractionDetection.cs
+++ Assets/DataSkopAR/Scripts/Core/UI/UIInteractionDetection.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 namespace DataskopAR {
 
 	public class UIInteractionDetection : MonoBehaviour {
+
+		public UnityEvent<Vector3> hasNotInteractedWithUI;
 
 		public static bool IsPointerOverUi { get; set; }
 
 		public static bool HasPointerStartedOverSwipeArea { get; set; }
 
 		public static bool HasPointerStartedOverSlider { get; set; }
+
+		[SerializeField] private UIDocument blockerDoc;
+
+		private VisualElement blockerRoot;
+
+		private void Awake() {
+			blockerRoot = blockerDoc.rootVisualElement;
+			blockerRoot.RegisterCallback<PointerUpEvent>(OnPointerDown);
+		}
+
+		private void OnPointerDown(PointerUpEvent e) {
+			Vector2 processedPos = new(e.position.x, blockerRoot.resolvedStyle.height - e.position.y);
+			hasNotInteractedWithUI?.Invoke(processedPos);
+		}
 
 	}
 
