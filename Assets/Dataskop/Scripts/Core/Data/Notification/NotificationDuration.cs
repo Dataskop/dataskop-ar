dfd29e1b1bcca0489f48b02a96c82bd38ce4a4d1
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-07-02 08:50:38 +0200

    add new notification duration

--- Assets/Dataskop/Scripts/Core/Data/Notification/NotificationDuration.cs
+++ Assets/Dataskop/Scripts/Core/Data/Notification/NotificationDuration.cs
@@ -2,6 +2,7 @@ namespace Dataskop.Data {
 
 	public static class NotificationDuration {
 
+		public const float Flash = 1.5f;
 		public const float Short = 5f;
 		public const float Medium = 10f;
 		public const float Long = 30f;
