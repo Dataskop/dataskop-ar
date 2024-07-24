d4831f7912931d3e277062f80e1ee36cca789878
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-07-24 16:25:54 +0200

    drop unused usings

--- Assets/Dataskop/Scripts/Core/Entities/Visualizations/Bar/Bar.cs
+++ Assets/Dataskop/Scripts/Core/Entities/Visualizations/Bar/Bar.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Dataskop.Data;
 using Dataskop.Interaction;
-using Mapbox.Unity;
 using UnityEngine;
 
 namespace Dataskop.Entities.Visualizations {
