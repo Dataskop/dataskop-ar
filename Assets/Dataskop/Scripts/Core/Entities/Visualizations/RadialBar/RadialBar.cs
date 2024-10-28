acca428a3af530e7e119867220e944b603eda357
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-10-28 12:55:28 +0100

    add radial fill vis

---
+++ Assets/Dataskop/Scripts/Core/Entities/Visualizations/RadialBar/RadialBar.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Dataskop.Data;
+using Dataskop.Entities;
+using Dataskop.Entities.Visualizations;
+using Dataskop.Interaction;
+using UnityEngine;
+
+namespace Dataskop {
+
+	public class RadialBar : MonoBehaviour, IVisualization {
+
+		[Header("References")]
+		[SerializeField] private GameObject visObjectPrefab;
+		[SerializeField] private Transform visObjectsContainer;
+
+		public IVisObject[] VisObjects { get; set; }
+
+		public IVisObject FocusedVisObject { get; }
+
+		public DataPoint DataPoint { get; }
+
+		public VisualizationOption VisOption { get; set; }
+
+		public VisHistoryConfiguration VisHistoryConfiguration { get; set; }
+
+		public bool IsSelected { get; }
+
+		public bool HasHistoryEnabled { get; }
+
+		public Transform VisOrigin { get; set; }
+
+		public MeasurementType[] AllowedMeasurementTypes { get; set; }
+
+		public Vector3 Offset { get; }
+
+		public VisualizationType Type { get; set; }
+
+		public event Action SwipedDown;
+
+		public event Action SwipedUp;
+
+		public event Action<int> VisObjectHovered;
+
+		public event Action<int> VisObjectSelected;
+
+		public event Action<int> VisObjectDeselected;
+
+		public event Action<IVisObject> FocusedVisObjectChanged;
+
+		public void Initialize(DataPoint dp) {
+			throw new NotImplementedException();
+		}
+
+		public void OnTimeSeriesToggled(bool isActive) {
+			throw new NotImplementedException();
+		}
+
+		public void OnFocusedIndexChanged(int index) {
+			throw new NotImplementedException();
+		}
+
+		public void OnSwipeInteraction(PointerInteraction pointerInteraction) {
+			throw new NotImplementedException();
+		}
+
+		public void OnMeasurementResultRangeUpdated() {
+			throw new NotImplementedException();
+		}
+
+		public void OnMeasurementResultsUpdated(int newIndex) {
+			throw new NotImplementedException();
+		}
+
+		public void ApplyStyle(VisualizationStyle style) {
+			throw new NotImplementedException();
+		}
+
+		public void Despawn() {
+			throw new NotImplementedException();
+		}
+
+	}
+
+}
